7b8d2dfd81bc0a3a2d8f176bb28ec0b6c0058442
Dugan <knolldug@grinnell.edu>
2014-05-12 14:18:53 -0700

    Added Client
    Now the website can load images!

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $.get( 'templates/twitter-feed.rac').then( function ( template ) {
     insta = new instagramFeed({
       el: 'template-target',
       template: template,
-      clientID: '',
+      clientID: 'fd88310566744275a3d68092d9c175d1',
       hashtag: 'webdesign'
     });//Ractive init
 });//Get Template
